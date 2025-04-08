9ad985cfaded219c1b498b5324e1589dd63633d4
LandryLuamba <lvewenda@msudenver.edu>
2025-04-08 08:53:13 -0600

    This is the working test for issue #51

--- HangmanGameTest.java
+++ HangmanGameTest.java
@@ -1,19 +1,23 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 import java.util.Collections;
+/**
+* generating test for selecting a random word from an internal list
+*@Author Landry Vewenda
+*@Author Efream Fsahaye
 
+*/
 
 
 public class HangmanGameTest{
 	public static String getRandomWord(List<String> words){
-		// if (words == null || words.isEmpty()){
-		//	return null;
-		//}
+		
+		if (words == null || words.isEmpty()){
+			return null;
+		}
 		Random random = new Random();
 		int randomIndex = random.nextInt(words.size());
 		return words.get(randomIndex);
@@ -22,6 +26,7 @@ public class HangmanGameTest{
 	void ValidNonEmptyListOfWords(){
 		List<String> words = Arrays.asList("java", "python", "kotlin");
 		String selectedWord = getRandomWord(words);
+		
 		
 	}
 	@Test
