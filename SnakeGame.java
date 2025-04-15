661c922fd1ad1ca618a64e6f2d5279ce487f8247
Spencer-Church <s.church5606@gmail.com>
2025-04-15 15:31:16 +0000

    Added a startup description for SnakeGame.java

--- SnakeGame.java
+++ SnakeGame.java
@@ -14,7 +14,11 @@ import java.util.Optional;
 class SnakeGame implements Game {
     public String getName() { return "Snake"; }
     public Optional<Integer> play() {
-        System.out.println("[Playing Snake - Placeholder]");
+	System.out.println("This is a simple console version of the Snake game.");
+	System.out.println("The player controls a snake that moves around a grid,");
+	System.out.println("collecting food and growing in length.");
+	System.out.println("The game ends if the snake runs into itself");
+	System.out.println("or the edge of the grid");
         return Optional.empty();
     }
 }
