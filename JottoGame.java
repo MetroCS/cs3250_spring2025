d592d2ef346f211c3aca11854c57c638589bcb62
TylerAndreasen <45052003+TylerAndreasen@users.noreply.github.com>
2025-04-17 10:13:41 -0600

    Reverted two println()_s to print()s. Added a space at the end of the example description text to make the Enter guess message more readable. Reverted stray new line.

--- JottoGame.java
+++ JottoGame.java
@@ -52,9 +52,12 @@ public class JottoGame implements Game {
 
     @Override
     public Optional<Integer> play() {
-        System.out.println("[Jotto] Guess the secret "
-                           + WORD_LENGTH + "-letter word. "
-                           + "You will be shown how many letters matched.");
+        System.out.print("[Jotto] Guess a secret word of "
+                           + WORD_LENGTH + " letters. "
+                           + System.lineSeparator()
+                           + "You will be shown how many letters matched."
+                           + System.lineSeparator()
+                           + "Ex: \"BRICK\" matches \"BUILD\" by two letters: \"B\" and \"I\". ");
         int attemptsLeft = MAX_GUESSES;
 
         while (attemptsLeft > 0) {
@@ -62,10 +65,7 @@ public class JottoGame implements Game {
             String guess = scanner.nextLine().trim().toUpperCase();
 
             if (guess.length() != WORD_LENGTH || !guess.matches("[A-Z]+")) {
-                System.out.println("Invalid input. "
-                                   + "Please enter a "
-                                   + WORD_LENGTH + "-letter word "
-                                   + "with alphabetic characters only.");
+                System.out.println("Invalid input. Please enter a "+ WORD_LENGTH + "-letter word with alphabetic characters only.");
                 continue;
             }
 
