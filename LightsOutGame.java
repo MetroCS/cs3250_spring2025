ad814545774cb7a2fd9ab7cdda03046bb2527101
Marina Chaparro <marinachaparro15@gmail.com>
2025-04-17 08:52:05 -0600

    Updated description for 'Lights Out' game. Closes: #17

--- LightsOutGame.java
+++ LightsOutGame.java
@@ -13,10 +13,10 @@ import java.util.Optional;
 class LightsOutGame implements Game {
     public String getName() { return "Lights Out"; }
     public Optional<Integer> play() {
-	System.out.println("Welcome to Lights out!");
-	System.out.println("The game includes a grid with various tiles that have an 'on' or 'off' function.");
+	System.out.println("Welcome to Lights Out!");
+	System.out.println("Lights Out includes a grid with tiles that have an 'on' or 'off' function.");
+	System.out.println("Selecting a tile will change its state. It can also change the state of its neighboring tiles. Turning them 'off' or 'on' as well.");
 	System.out.println("The goal is to light the entire grid in as few turns as possible. Good luck!");
-        System.out.println("[Playing Lights Out - Placeholder]");
         return Optional.empty();
     }
 }
