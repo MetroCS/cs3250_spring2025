a288447e36454d0f3dfa3437efce22c9ad990b13
LandryLuamba <lvewenda@msudenver.edu>
2025-04-17 10:14:03 -0600

    Fixed bug for Number Guess Game #74

--- NumberGuessGame.java
+++ NumberGuessGame.java
@@ -11,13 +11,23 @@ import java.util.Optional;
  * @version 1
  */
 class NumberGuessGame implements Game {
-    public String getName() { return "Number Guess"; }
+    /**
+    *gets the string name.
+    *@return Number Guess.
+    */
+    public String getName() {
+        return "Number Guess";
+    }
+    /**
+    *Holding a value of primitive type int.
+    *@return an empty optional instance.
+    */
     public Optional<Integer> play() {
         System.out.println("[Playing Number Guessing Game]");
         System.out.println("This is a simple game where you"
-                           + "try and guess the number I pick.");
+                           + " try and guess the number I pick.");
         System.out.println("If you are too high or too low I will let"
-                           + "you know. Try to do it in the fewest attempts!");
+                           + " you know. Try to do it in the fewest attempts!");
         return Optional.empty();
     }
 }
