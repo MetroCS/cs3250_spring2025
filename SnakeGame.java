544501596706c6896e7f0f64008a98819c744eb6
Spencer-Church <s.church5606@gmail.com>
2025-04-22 14:59:37 +0000

    Fixed Checkstyle issues in SnakeGame.java

--- SnakeGame.java
+++ SnakeGame.java
@@ -12,13 +12,17 @@ import java.util.Optional;
  * @version 1
  */
 class SnakeGame implements Game {
-    public String getName() { return "Snake"; }
+    public String getName()
+    { return "Snake";
+}
     public Optional<Integer> play() {
-	System.out.println("This is a simple console version of the Snake game.");
-	System.out.println("The player controls a snake that moves around a grid,");
-	System.out.println("collecting food and growing in length.");
-	System.out.println("The game ends if the snake runs into itself");
-	System.out.println("or the edge of the grid");
-        return Optional.empty();
+    System.out.println("This is a simple console"+
+                       " version of the Snake game.");
+    System.out.println("The player controls a snake"+
+                       " that moves around a grid,");
+    System.out.println("collecting food and growing in length.");
+    System.out.println("The game ends if the snake runs into itself");
+    System.out.println("or the edge of the grid");
+    return Optional.empty();
     }
 }
