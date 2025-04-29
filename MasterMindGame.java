865404ee213b67a54718ec4b153a5e0361aecd31
Fernando Ruiz <freshr1515@gmail.com>
2025-04-29 09:20:48 -0600

    Description Added

--- MasterMindGame.java
+++ MasterMindGame.java
@@ -8,12 +8,22 @@ import java.util.Optional;
  * the correct position and how many are correct but in the wrong position.
  *
  * Consider using arrays or strings to store and compare codes.
- * @version 1
+ * @version 2
  */
 class MasterMindGame implements Game {
-    public String getName() { return "MasterMind"; }
-    public Optional<Integer> play() {
-        System.out.println("[Playing MasterMind - Placeholder]");
+    @Override
+    public String getName() {
+    return "MasterMind";
+    }
+    @Override
+ public Optional<Integer> play() {
+
+    System.out.println("You are now playing Mastermind");
+    System.out.println("The app selects a sequance of symbols");
+    System.out.println("The player tries to guess the sequance");
+    System.out.println("You have a fixed number of attepmts to guess");
+    System.out.println("When you guess correctly you win, have fun!");
+
         return Optional.empty();
     }
 }
